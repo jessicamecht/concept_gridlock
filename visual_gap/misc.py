458231fd6fcd4a0beeeeb8e30b327af933d6135f
Yan An <gavinyan97@gmail.com>
2022-11-02 09:35:54 -0700

    misc code to understand data.

--- visual_gap/misc.py
+++ visual_gap/misc.py
@@ -1,7 +1,7 @@
 '''
 misc code to understand data, try out libraries
 '''
-
+import sys
 '''
 processed file
 '''
@@ -55,19 +55,89 @@ read json
 # print(a['frames'][0]['annos']['boxes_2d']['cam01'][0])
 
 
+'''
+read once data processed by Noveen
+'''
+# import numpy as np
+# from collections import defaultdict
+# flat_data = np.load("../data/once/metadata.npz")['data']     
+# dict_data = defaultdict(list)
+# print (len(flat_data))
+# weather_map = {}
+# for sequence_id, timestamp, gap, speed, weather, image_index in flat_data:
+#     if weather not in weather_map: weather_map[weather] = len(weather_map)
+# print(weather_map)
+# for sequence_id, timestamp, gap, speed, weather, image_index in flat_data:
+#     dict_data[sequence_id].append([
+#         float(gap),
+
+#         # Logging policy dependent
+#         [
+#             0, # time-gap NOTE: need to calculate using physics
+#             float(speed), # self-speed
+#             0 # current self-acceleration
+#         ],
+
+#         # Independent of the logging policy
+#         [
+#             0, # Leader-speed
+#             weather_map[weather], # weather information not available,
+#             0 # self-vehicle-name
+#         ],
+
+#         timestamp,       
+#     ])
+
+# data = []
+# for sequence_id in dict_data:
+#     # The file is already sorted by the timestamp, but just making sure
+#     user_history = sorted(dict_data[sequence_id], key = lambda x: x[-1])
+#     # Removing the timestamp now as we don't need it anymore
+#     user_history = list(map(lambda x: x[:-1], user_history))
+#     data.append(user_history)
+
+# print(data[0][:10])
+# x = [ len(i) for i in data]
+# print ('seq_len', x)
+# print (sum(x))
+# image_paths = np.load("../data/once/metadata.npz")['image_paths']   
+# print (len(image_paths))
+# # print(len(dict_data))
+# # print(dict_data.keys())
+# total = 0
+# for i in x:
+#     total += i
+#     print (image_paths[total-1])
 
 
+### find sequence in h5py
+
+# sequence_ids = ['000076', '000080', '000092', '000104', '000113', '000121', '000027', '000028', '000112', '000201']
+# import h5py
+
+# path = "/data1/jessica/data/toyota/once_data_w_lanes_compressed.hfd5"
+# # # path = "/data1/jessica/data/toyota/once_data_w_lanes_compressed_train.hfd5"
+# # path = "/data1/jessica/data/toyota/once_data_w_lanes_compressed_train_resized_images.hfd5"
+
+# with h5py.File(path, "r") as f:
+#     print ([s for s in f.keys()])
+#     for i, seq_key in enumerate(sequence_ids):
+#         iter_dict = {}
+#         keys_ = f[seq_key].keys()
+#         print (seq_key)
+#         print (f[seq_key]['pos'])
 
 
+'''
 
+'''
+import numpy as np
+from collections import defaultdict
+data = np.load("../data/once/old_metadata.npz")
+flat_data = data['data'] 
+image_paths = data['image_paths'] 
 
-
-
-
-
-
-
-
+print (flat_data[0])
 
 
 
