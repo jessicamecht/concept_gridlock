a4cf1b317b4d44a146cfeee772c832b5d560582b
Jessica <j.m.echt@gmail.com>
2023-06-05 05:28:53 -0700

    change order

--- run_experiments_concept.sh
+++ run_experiments_concept.sh
@@ -1,9 +1,11 @@
-python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task angle -bs 1
-python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task angle -bs 1
-python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 3  -max_epochs 200 -task multitask -bs 1
-python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 3  -max_epochs 200 -task multitask -bs 1
-python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 2  -max_epochs 200 -task distance -bs 1
-python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 2  -max_epochs 200 -task distance -bs 1
+python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task distance -bs 4
+python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task distance -bs 4
+
+python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task multitask -bs 4
+python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task multitask -bs 4
+
+python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task angle -bs 4
+python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task angle -bs 4
 
 
 
