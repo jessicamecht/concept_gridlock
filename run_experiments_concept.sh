8001ca77500caf2563ec2b4659c33422dfcda96f
jessica <j.m.echt@gmail.com>
2023-06-05 12:04:02 -0700

    rum comma

--- run_experiments_concept.sh
+++ run_experiments_concept.sh
@@ -1,10 +1,10 @@
-python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task distance -bs 4
+#python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task distance -bs 4
 python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task distance -bs 4
 
-python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task multitask -bs 4
+#python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task multitask -bs 4
 python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task multitask -bs 4
 
-python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task angle -bs 4
+#python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task angle -bs 4
 python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task angle -bs 4
 
 
