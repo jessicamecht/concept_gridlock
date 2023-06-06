b80082459cb71ad080a89b2376d231e91777fd40
Jessica <j.m.echt@gmail.com>
2023-06-06 01:31:20 -0700

    add new exp

--- new_exp.sh
+++ new_exp.sh
@@ -2,4 +2,4 @@ python3 main.py -dataset comma -backbone clip -ground_truth normal -train -gpu_n
 python3 main.py -dataset comma -backbone clip -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task  distance -bs 2  -dataset_path /data1/shared/jessica/data1/data/toyota/
 python3 main.py -dataset nuscenes -backbone clip -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task  multitask -bs 32  -dataset_path /data1/shared/jessica/data1/data/toyota/
 
-python3 main.py -dataset comma -backbone resnet -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task  multitask -bs 1  -dataset_path /data1/shared/jessica/data1/data/toyota/
+python3 main.py -dataset nuscenes -backbone resnet -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task  multitask -bs 1  -dataset_path /data1/shared/jessica/data1/data/toyota/
