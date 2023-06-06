67e219f9145985c94abcb759708029ee9294cb47
Jessica <j.m.echt@gmail.com>
2023-06-06 04:48:49 -0700

    changes to module

--- run_experiments_concept.sh
+++ run_experiments_concept.sh
@@ -1,4 +1,5 @@
 #python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task distance -bs 4
+
 python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task distance -bs 4
 
 #python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task multitask -bs 4
@@ -6,7 +7,6 @@ python3 main.py -dataset comma -backbone none -concept_features -ground_truth no
 
 #python3 main.py -dataset nuscenes -backbone none -concept_features -ground_truth normal -train -gpu_num 0  -max_epochs 200 -task angle -bs 4
 python3 main.py -dataset comma -backbone none -concept_features -ground_truth normal -train -gpu_num 1  -max_epochs 200 -task angle -bs 4
-
 
 
 
